41073fdb4993a968f86b510bb1be368f491d692d
Russell Cohen <russell.r.cohen@gmail.com>
2018-01-26 22:10:50 -0800

    fix sha

--- sumoshell.rb
+++ sumoshell.rb
@@ -2,7 +2,7 @@ class Sumoshell < Formula
   desc "A command line tool to analyze logfiles"
   homepage "https://github.com/SumoLogic/sumoshell"
   url "https://github.com/SumoLogic/sumoshell/releases/download/v0.5.0/sumoshell-osx.zip"
-  sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+  sha256 "ec31abe65ee1c8887280bcfab989c6ce7f08a787d01978d9141f1dca9ad53849"
   version "0.5.0"
 
   # depends_on "curl"
